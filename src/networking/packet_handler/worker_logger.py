82f42a62110ecc5186ac616edb7c2f4f911898dc
mdashlw <mdashlw@gmail.com>
2020-04-13 14:22:18 +0300

    Try to fix login handling

--- src/networking/packet_handler/worker_logger.py
+++ src/networking/packet_handler/worker_logger.py
@@ -14,34 +14,28 @@ class WorkerLogger(threading.Thread):
         destroy_entities = DestroyEntities().read(packet.packet_buffer)
         for entity_id in destroy_entities.Entities:
             if entity_id in self.parent.log[SpawnEntity.id]:
-                #print("Removed entity ID: %s" % entity_id, self.parent.log[SpawnEntity.id].keys(),
-                #      flush=True)
                 del self.parent.log[SpawnEntity.id][entity_id]  # Delete the entity
-
-    # def chunk_unload(self, packet):
-    #     unload_chunk = UnloadChunk().read(packet.packet_buffer)
-    #     chunk_key = (unload_chunk.ChunkX, unload_chunk.ChunkY)
-    #     if chunk_key in self.parent.log[ChunkData.id]:
-    #         del self.parent.log[ChunkData.id][chunk_key]
-    #         print("UnloadChunk", unload_chunk.ChunkX, unload_chunk.ChunkY)
 
     def chunk_load(self, packet):
         chunk_data = ChunkData().read(packet.packet_buffer)
         chunk_key = (chunk_data.ChunkX, chunk_data.ChunkY)
         if chunk_key not in self.parent.log[packet.id]:
             self.parent.log[packet.id][chunk_key] = packet
-        # print("ChunkData", chunk_data.ChunkX, chunk_data.ChunkY)
 
     def spawn_entity(self, packet):
         spawn_entity = SpawnEntity().read(packet.packet_buffer)
         if spawn_entity.EntityID not in self.parent.log[SpawnEntity.id]:
             self.parent.log[SpawnEntity.id][spawn_entity.EntityID] = packet
-            #print("Added entity ID: %s" % spawn_entity.EntityID, self.parent.log[SpawnEntity.id].keys(),
-            #  flush=True)
 
     def process_packet(self, packet):
         if packet.id in self.parent.connection.join_ids:
             self.parent.log[packet.id] = packet
+        elif packet.id == ChunkData.id:
+            self.chunk_load(packet)
+        elif packet.id in SpawnEntity.ids:
+            self.spawn_entity(packet)
+        elif packet.id == DestroyEntities.id:
+            self.destroy_entities(packet)
         elif packet.id == KeepAlive.id and not self.parent.connection.client_connection: # KeepAlive Clientbound
             keep_alive = KeepAlive().read(packet.packet_buffer)
             self.parent.connection.send_packet(keep_alive)
